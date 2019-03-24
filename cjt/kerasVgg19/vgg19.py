0b0f3202e5cc12b56f25f2289149a366d0355847
hzd <hzd138@qq.com>
2019-03-24 16:05:15 +0800

    统计 分组 映射生成

--- cjt/kerasVgg19/vgg19.py
+++ cjt/kerasVgg19/vgg19.py
@@ -7,25 +7,27 @@ import tensorflow as tf
 import numpy as np
 import os
 
+
 def car_vgg_model():
-    base_model = VGG19(weights='/Users/cai/personal/car/tfSource/car-dnn-classifier/cjt/vgg19_weights_tf_dim_ordering_tf_kernels.h5',
-                       include_top=False) #不将最顶端的三层全连接include进来
+    base_model = VGG19(
+        weights='/Users/cai/personal/car/tfSource/car-dnn-classifier/cjt/vgg19_weights_tf_dim_ordering_tf_kernels.h5',
+        include_top=False)  # 不将最顶端的三层全连接include进来
 
     x = base_model.output
     x = Dense(4096, activation='relu')(x)
     x = Dense(4096, activation='relu')(x)
-    predictions = Dense(100,activation="softmax")(x) #softmax函数输出100个分类
+    predictions = Dense(100, activation="softmax")(x)  # softmax函数输出100个分类
 
-    model = Model(inputs=base_model.input,outputs=predictions)
+    model = Model(inputs=base_model.input, outputs=predictions)
 
-    #只训练最top的三层，冻结vgg19的其他层
+    # 只训练最top的三层，冻结vgg19的其他层
     for layer in base_model.layers:
         layer.trainable = False
 
-    #编译模型
+    # 编译模型
     model.compile(optimizer="rmsprop", loss="categorical_crossentropy")
 
-    #转化模型成estimator model ，方便tensorflow训练
-    model_dir = os.path.join(os.path.abspath(__file__),"vggTrain") # 存放在当前路径的vggTrain文件夹下面
-    estimator_model = tf.keras.estimator.model_to_estimator(model,model_dir=model_dir)
+    # 转化模型成estimator model ，方便tensorflow训练
+    model_dir = os.path.join(os.path.abspath(__file__), "vggTrain")  # 存放在当前路径的vggTrain文件夹下面
+    estimator_model = tf.keras.estimator.model_to_estimator(model, model_dir=model_dir)
     return estimator_model
---
+++ hzd/f/car_statistic_choose_and_mapping.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+import os
+import shutil
+
+file_set = []
+
+root_dir = "D:\\trainData\\trainData"
+unsolved_list = os.listdir(root_dir)
+
+brand_count = {}
+
+
+def output_data_and_mapping(items, num, output_path):
+    mapper = {}
+    cursor = 0
+    for i_key, i_value in items:
+        mapper[cursor] = i_key
+        source_date_path = root_dir + os.sep + i_key
+        target_data_path = output_path + os.sep + str(cursor)
+        copied_num = 0
+        if not os.path.exists(target_data_path):
+            os.makedirs(target_data_path)
+        for file in os.listdir(source_date_path):
+            file_path = source_date_path + os.sep + file
+            shutil.copy(file_path, target_data_path + os.sep + file)
+            copied_num += 1
+            if copied_num == num:
+                break
+        cursor += 1
+    out_mapper_file = open(output_path + os.sep + 'mapper', 'w')
+    for t_key, t_value in mapper:
+        out_mapper_file.write('%s:%s\n' % (t_key, t_value))
+    out_mapper_file.close()
+
+
+def travel_dir(cur_dir):
+    dir_list = os.listdir(cur_dir)
+    cur_dir_file_count = 0
+    for element in dir_list:
+        element_path = cur_dir + os.sep + element
+        # 是文件就计数
+        if os.path.isfile(element_path):
+            cur_dir_file_count = cur_dir_file_count + 1
+        else:
+            travel_dir(element_path)
+    if cur_dir_file_count != 0:
+        brand_name = cur_dir[len(root_dir) + 1:]
+        # print("%s = %d" % (brand_name, cur_dir_file_count))
+        brand_count[brand_name] = cur_dir_file_count
+        if len(brand_count) % 50 == 0:
+            print(len(brand_count))
+
+
+travel_dir(root_dir)
+print('traveled total:%d' % (len(brand_count)))
+sorted_list = sorted(brand_count.items(), key=lambda d: d[1], reverse=True)
+print('sorted')
+while True:
+    num_str = input('切分的最少的训练集张数')
+    num = int(num_str)
+    found_num = 0
+
+    for key, value in sorted_list:
+        if value >= num:
+            found_num = found_num + 1
+        else:
+            break
+    print('训练集张数至少有 %d 张的车型数为： %d' % (num, found_num))
+    will_output_data = input('是否导出数据及生成映射关系? y/n')
+    if will_output_data.lower() == 'y':
+        output_path = input('输出到的路径')
+        output_data_and_mapping(sorted_list[:found_num], num, output_path)
+        print('done')
+        break
+    else:
+        print('重新选择')
